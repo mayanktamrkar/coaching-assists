0e7cac7418711bd13c4182f46cfaa5a9fd1aafe3
mayank tamrkar <mayank@mindpathtech.com>
2023-09-03 17:19:25 +0530

    add redux setup

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+"use client";
+import { EXAMPLE_DATA, exampleActions } from "@/datastore";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-const page = () => {
+const Page = () => {
+  const dispatch = useDispatch();
+  const exampleData = useSelector(EXAMPLE_DATA);
+  console.log("✌️exampleData --->", exampleData);
   return (
-    <div> signup page</div>
-  )
-}
+    <div
+      onClick={() => {
+        dispatch(exampleActions.decrement());
+      }}
+    >
+      signup page{exampleData}
+    </div>
+  );
+};
 
-export default page
+export default Page;
---
+++ src/datastore/reducer/example.reducer.ts
@@ -0,0 +1,25 @@
+// redux/counterSlice.js
+
+import { createSlice } from "@reduxjs/toolkit";
+interface IInitailState {
+  value: number;
+}
+const initialState: IInitailState = {
+  value: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+  },
+});
+
+export const exampleActions = {...counterSlice.actions}
+export default counterSlice.reducer;
